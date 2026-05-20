99e8bac0c12afe083a1e9da2e335cf3c253961e4
Rebeca <rasrebeca@gmail.com>
2026-05-20 14:41:21 -0300

    Adiciona primeiros exercicios

---
+++ Exercicios/2.1.py
@@ -0,0 +1,10 @@
+# Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python:
+#a) 10 + 20 × 30
+#b) 42 ÷ 30
+#c) (94 + 2) × 6 - 1
+
+a = 10 + 20 * 30
+b = 42 / 30
+c = (94 + 2) * 6 - 1
+
+print(a, b, c)
---
+++ Exercicios/2.3.py
@@ -0,0 +1,17 @@
+#1) Faça um programa que exiba seu nome na tela.
+#2) Escreva um programa que exiba o resultado de 2a × 3b, onde “a” vale 3 e “b” vale 5.
+#3) Faça um programa que calcule a soma de três variáveis.
+#4) Faça um programa que calcule um aumento de 15% para um salário de R$ 1750.
+
+print('Rebeca')
+
+a = 3
+b = 5 
+print((2 * a) * (3 * b))
+
+c = 9
+print(a + b + c)
+
+salario = 750
+aumento = 15 
+print(salario + (salario * aumento / 100))
